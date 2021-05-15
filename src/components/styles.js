5b1f66a9cf3c5632cb576ead28965ed6bf98a20e
Luma8 <lucasrobbot@gmail.com>
2021-05-15 12:40:25 -0300

    Login update

--- src/components/styles.js
+++ src/components/styles.js
@@ -4,10 +4,42 @@ export const Container = styled.View`
    flex:1;
    justify-content: center;
    align-items: center;
-   background-color: #121212;
 `;
 
 export const Title = styled.Text`
     font-size: 10px;
-    color: #yellow;
+    color: #Yellow;
+`;
+
+export const Title1 = styled.Text`
+    font-size: 24px;
+    color: #00081d;
+    font-family: "TypoGraphica";
+    margin-bottom: 60;
+    margin-top: 5;
+`;
+
+export const Botao = styled.Button`
+    color: aliceblue;
+    background-color: #102660;
+    border-color: #102660;
+    border-radius: 30px;
+    padding: 1% 10%;
+    font-family: "TypoGraphica" !important;
+    border-radius: 30px !important;
+`;
+
+export const Div1 = styled.View`
+
+`;
+export const Inputs = styled.TextInput`
+   margin-top: 3; 
+   margin-bottom: 3;
+   background-color: #102660 !important;
+   border-color: #102660 !important;
+   border-radius: 40px !important;
+   padding: 5% 20% !important;
+   font-family: "Roboto" !important;
+   margin-top: auto !important;
+   margin-bottom: 15;
 `;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Container, Title } from '../components/styles';
-import { Button } from 'react-native';
+import { Container, Title, Title1, Botao, Div1, Inputs } from '../components/styles';
+import { Button, TextInput, Image, email, password } from 'react-native';
 
 export default function Login({ navigation }){
     return(
         <Container>
+            <Div1>
+                <Title1 h1>
+                    Fazer login
+                </Title1>
+                <Inputs
+                   placeholder="Email"
+                />
+                 <Inputs
+                   placeholder="Senha"
+                />
+                
+            </Div1>
             <Title>
                 LOGIN
             </Title>
-            <Button
-                title="Register"
+            <Botao
+                title="Entra"
                 onPress={() => navigation.navigate('Register')} 
+                color="#21429d"
             />
         </Container>
     );
-
 }
